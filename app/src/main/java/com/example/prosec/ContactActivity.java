371fff88c752b51340157f15ec80490c877ebca0
jadha <jadhavrkiran200@gmail.com>
2022-05-14 17:09:36 +0530

    layout

---
+++ app/src/main/java/com/example/prosec/ContactActivity.java
@@ -0,0 +1,25 @@
+package com.example.prosec;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Adapter;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class ContactActivity {
+}
